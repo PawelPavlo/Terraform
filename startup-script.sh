cdcd7cdd9dea8dca79a69d20be8820c260090e2a
Pawel Pavlo <vihpl@gmail.com>
2021-08-05 22:31:31 +0000

    last

--- startup-script.sh
+++
@@ -1,5 +0,0 @@
-export HOME=/root
-apt-get update && apt-get install -yq ansible git
-git clone -b master https://github.com/PawelPavlo/Ansible.git /opt/ansible
-cd /opt/ansible/ansibleproject && ansible-playbook playbook.yml
-
